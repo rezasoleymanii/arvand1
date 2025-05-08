0932a2e1cbe81af7f0a6d923c177a395e6f63f88
rezasoleymani <rezasoleiii@gmail.com>
2025-05-08 23:55:11 +0330

    adit js css html

--- js/arvand.js
+++ js/arvand.js
@@ -1,18 +1,21 @@
-let item = document.querySelectorAll('.menu-container li a');
-let list = document.querySelector('.menu-container li .list')
+let item = document.querySelectorAll('.menu-container li');
+let list = document.querySelector('.menu-container li ul'); 
 item.forEach(function(item, index) {
     item.addEventListener('click' , function (e) {
+        
         e.preventDefault();
         
-        document.querySelector('.menu-container li a.selected').classList.remove('selected');
+        document.querySelector('.menu-container .selected').classList.remove('selected');
         this.classList.add('selected');
         
         let dataBox = this.getAttribute('data-content');
         
-        document.querySelector('.min-center li.selected').classList.remove('selected');
+        document.querySelector('.min-center .selected').classList.remove('selected');
         document.querySelector(".min-center li[data-content="+ dataBox +"]").classList.add('selected');
+        
         
 
     })
+    
 })
 

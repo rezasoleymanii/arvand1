ed55231030467050a7e290977f6669a88f40f25c
rezasoleymani <rezasoleiii@gmail.com>
2025-05-11 06:13:19 +0330

    adit

--- js/arvand.js
+++ js/arvand.js
@@ -1,11 +1,11 @@
-let item = document.querySelectorAll('.menu-container li');
-let list = document.querySelector('.menu-container li ul'); 
+let item = document.querySelectorAll('.menu-container li ');
+let list = document.querySelectorAll('.menu-container li ul li ');
 item.forEach(function(item, index) {
     item.addEventListener('click' , function (e) {
         
         e.preventDefault();
         
-        document.querySelector('.menu-container .selected').classList.remove('selected');
+        document.querySelector('.menu-container li.selected').classList.remove('selected');
         this.classList.add('selected');
         
         let dataBox = this.getAttribute('data-content');
@@ -18,4 +18,19 @@ item.forEach(function(item, index) {
     })
     
 })
+list.forEach(function(list, index) {
+
+    list.addEventListener('click' , function (w) {
+        
+        document.querySelector('.menu-container li ul li.menu-selected').classList.remove('menu-selected');
+        this.classList.add('menu-selected');
+        
+        let dataBox = this.getAttribute('data-content');
+
+        document.querySelector('.min-center ul .menu-selected').classList.remove('menu-selected');
+        document.querySelector(".min-center ul li[data-content="+ dataBox +"]").classList.add('menu-selected');
+    })
+
+})
+
 
